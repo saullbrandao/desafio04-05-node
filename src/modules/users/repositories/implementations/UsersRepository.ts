2425643367e16d7da7cca873af993e714e6c9876
Saull Brandão <saull@outlook.com>
2022-01-11 09:31:44 -0300

    feat: turn an user as admin

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -44,9 +44,16 @@ class UsersRepository implements IUsersRepository {
     return user;
   }
 
-  // turnAdmin(receivedUser: User): User {
-  //   // Complete aqui
-  // }
+  turnAdmin(receivedUser: User): User {
+    const user = this.users.find((user) => user.id === receivedUser.id);
+
+    if (user) {
+      user.admin = true;
+      user.updated_at = new Date();
+    }
+
+    return user;
+  }
 
   list(): User[] {
     return this.users;
