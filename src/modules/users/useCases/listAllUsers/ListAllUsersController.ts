efb7313e07bc5a286ad5c9cd19dc4cbbf8d6b47f
Saull Brandão <saull@outlook.com>
2022-01-11 10:03:44 -0300

    feat: controller to list all users

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,19 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const user_id = Array.isArray(request.headers.user_id)
+      ? request.headers.user_id[0]
+      : request.headers.user_id;
+
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.json(users);
+    } catch (err) {
+      return response.status(400).json({
+        error: err.message || "Unexpected error.",
+      });
+    }
   }
 }
 
