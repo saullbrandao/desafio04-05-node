4b4e8a7682cefa8e3215d0b7ac1d00fd48af28ce
Saull Brandão <saull@outlook.com>
2022-01-11 09:51:41 -0300

    feat: controller to create a new user

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -11,16 +11,16 @@ usersRoutes.post("/", (request, response) =>
   createUserController.handle(request, response)
 );
 
-usersRoutes.patch("/:user_id/admin", (request, response) =>
-  turnUserAdminController.handle(request, response)
-);
+// usersRoutes.patch("/:user_id/admin", (request, response) =>
+//   turnUserAdminController.handle(request, response)
+// );
 
-usersRoutes.get("/:user_id", (request, response) =>
-  showUserProfileController.handle(request, response)
-);
+// usersRoutes.get("/:user_id", (request, response) =>
+//   showUserProfileController.handle(request, response)
+// );
 
-usersRoutes.get("/", (request, response) =>
-  listAllUsersController.handle(request, response)
-);
+// usersRoutes.get("/", (request, response) =>
+//   listAllUsersController.handle(request, response)
+// );
 
 export { usersRoutes };
